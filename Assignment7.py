e859a37056522246e90c894e42d83f90e2831154
gopikakv124 <81343838+gopikakv124@users.noreply.github.com>
2021-06-06 00:18:14 +0530

    Update Assignment7.py

--- Assignment7.py
+++ Assignment7.py
@@ -45,6 +45,7 @@ def todb():
  
  #receiving data from OpenWeatherMap
  response = requests.get(api_url).json()
+ #response = urlopen(api_url).read()
  
  #Cleaning the data to make it look tidy
  currentWeather = response['current']
